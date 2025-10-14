a48f455ea1464fc0e6eb849cfdb562fc164ca97e
M4l3k00 <malekmerdassi33@gmail.com>
2025-10-14 21:00:20 +0200

    Add files via upload

---
+++ esercizi_variabili.py
@@ -0,0 +1,20 @@
+
+print('Es. 1')
+#Crea una variabile x con valore 7 e una y con valore 3. Stampa la somma dei due numeri
+x = 7; y = 3
+print(x+y)
+
+print('Es. 2')
+#Crea una variabile prezzo con valore 19.99 e una quantità con valore 3. Stampa il totale (prezzo × quantità).
+prezzo = 19.99; quantita = 3
+print(prezzo*quantita)
+
+print('Es. 3')
+#Crea una variabile numero con valore 10. Stampa True se il numero è maggiore di 5, altrimenti False.
+numero = 10
+print(numero>5)
+
+print('Es. 4')
+#Prova a combinare un int e un float: assegna a a = 5 e b = 2.5, poi calcola la loro somma.
+a, b = 5, 2.5
+print(a+b) 
