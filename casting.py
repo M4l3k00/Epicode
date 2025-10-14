72e792d3c875bbaaec54356be73adfb256ed0819
M4l3k00 <malekmerdassi33@gmail.com>
2025-10-14 21:17:22 +0200

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,13 @@
+print('Es. 1')
+#Chieda all’utente di inserire un numero intero.
+intero = int(input('inserisci un numero intero: '))
+print('il numero che hai inserito è ', intero)
+
+print('Es. 2')
+#• Converta questo numero in numero decimale (float) e lo stampi.
+print('in decimale vale: ', (float(intero)))
+
+print('Es. 3')
+#Converta lo stesso numero in stringa e lo stampi insieme a un messaggio.
+print('formato stringa: ', str(intero), '  ', type(str(intero)))
+
