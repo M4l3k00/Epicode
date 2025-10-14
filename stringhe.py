b2367f6b8d6aa16ae78495695949adcf268b623b
M4l3k00 <malekmerdassi33@gmail.com>
2025-10-14 21:45:42 +0200

    Add files via upload

---
+++ stringhe.py
@@ -0,0 +1,10 @@
+print('Es. 1')
+#• Chiedi una frase e inverti l’ordine delle parole
+stringa = input('scrivi qui la tua stringa: ')
+print('stringa inverita: ', stringa[::-1])
+
+print('Es. 2')
+#Controlla se la frase è un palindromo (ignora spazi e maiuscole)
+stri = input('inserisci la stringa: ')
+irts = stri[::-1]
+print(stri == stri)
