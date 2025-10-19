172fc62da2913a0a750d652eb449f1152cd01614
M4l3k00 <malekmerdassi33@gmail.com>
2025-10-19 22:58:53 +0200

    Add files via upload

---
+++ liste.py
@@ -0,0 +1,8 @@
+lista_numeri = [1, 2, 3, 4, 5]
+print(lista_numeri)
+
+var_sos = input('inserisci numero da sostituire al 3: ')
+var_sos = float(var_sos)
+
+lista_numeri.insert(2, var_sos)
+print(lista_numeri)
